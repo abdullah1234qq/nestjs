4d9b9e8e08338d93441729205a12da7c638dff79
Abdullah Hussain <abdullah1234qq>
2025-07-02 11:36:02 +0500

    Innitial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { DatabadeModule } from './databade/databade.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, DatabadeModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
